2775a51f95e7a6529cb8ba7a83cf9139b9df8565
sechang <t@tt.tt>
2023-05-17 18:05:43 +0900

    day01

--- src/day01/TestClass01.java
+++ src/day01/TestClass01.java
@@ -1,5 +1,15 @@
 package day01;
 
 public class TestClass01 {
-
+	public static void main(String[] args) {
+		//print는 괄호안의 내용을 출력하는 기능이다
+		//System.out.println(1111);
+		/*
+		  여러줄을 주석 처리할 때
+		 */
+		System.out.println("안녕하세요");
+		System.out.println("반갑습니다");
+		System.out.print("111");
+		System.out.print("222");
+	}
 }
